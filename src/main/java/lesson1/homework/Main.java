57984e9b02d25071e780f4e607d1124d9c346a88
scrag <scrag1987@gmail.com>
2020-04-26 19:24:43 +0600

    task1

---
+++ src/main/java/lesson1/homework/Main.java
@@ -0,0 +1,7 @@
+package lesson1.homework;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("https://github.com/RbkGh/Free-Algorithm-Books/tree/master/book");
+  }
+}
